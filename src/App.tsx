b2cd97794b44864ef470240e18c149a97981822b
Matheus Lemes <matheus311095@yahoo.com.br>
2023-06-18 13:46:11 -0300

    Iniciando form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hellow World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
